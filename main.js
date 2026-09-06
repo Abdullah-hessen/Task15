80660d56bf7894731a9939cec3de7b849841ad08
Abdullah Mahmoud <abdullah.mahmoud.hessen@gmail.com>
2026-09-06 23:49:38 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+// Problem Solving Question 1
+
+// function integerCounter() {
+//   let num = prompt("Enter an integer number: ");
+
+//   console.log("the integer has " + num.length + " digits!");
+// }
+// integerCounter();
+
+// Problem Solving Question 2
+
+// function reverseNumber() {
+//   let num = prompt("Enter an integer number: ");
+//   let count = num.length - 1;
+//   let reversedNumber = "";
+
+//   for (let i = 0; count <= i; count--) {
+//     reversedNumber += num[count];
+//   }
+//   console.log(reversedNumber);
+// }
+// reverseNumber();
+
+// Choose the correct answer:
+
+//    1-  B. 10
+//    2-  A. undefined
+//    3-  D. error
+//    4-  D. error
+//    5-  B. Hello
+//    6-  A. undefined
+//    7-  A. undefined - 2
+//    8-  A. 30 - 20 - 10
+//    9-
